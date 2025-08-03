817e6cb10d8c736135bc88302df7e5c782984027
Takaesi <takaesi@yandex.ru>
2025-08-03 12:00:51 +0300

    + view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class NewsList(ListView):
+    model = Post
+    ordering = 'post_type'
+    template_name = 'news/news_list.html'
+    context_object_name = 'news'
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'news/news_detail.html'
+    context_object_name = Post
